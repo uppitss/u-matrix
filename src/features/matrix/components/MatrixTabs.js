01afe4045c368f59a6688e3c5d133b81bf2c7f23
uppitss <uppitss@gmail.com>
2025-05-27 15:07:27 +0300

    Добавили удаление вкладок. Добавили матрицу эйзенхауэра

--- src/features/matrix/components/MatrixTabs.js
+++ src/features/matrix/components/MatrixTabs.js
@@ -1,8 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {saveAppData} from "../services/ioService";
 import {UTab} from "../model/UTab";
-import {Button, Flex, Tabs} from "@chakra-ui/react";
-import {LuPlus, LuX} from "react-icons/lu";
+import {Button, CloseButton, Flex, Grid, GridItem, Heading, Tabs} from "@chakra-ui/react";
+import {LuPlus} from "react-icons/lu";
 
 
 const MatrixTabs = (props) => {
@@ -46,23 +45,26 @@ const MatrixTabs = (props) => {
             <Tabs.Root
                 key={tabs.length}
                 value={activeTab}
+                fitted
                 onValueChange={(e) => setActiveTab(e.value)}>
                 <Tabs.List>
                     {
                         tabs.map((tab, index) => {
-                            return <Tabs.Trigger value={tab.id}>
+                            return <Tabs.Trigger
+                                key={"tabs_trigger_" + tab.id}
+                                value={tab.id}>
                                 {tab.title}
-                                <Button
-                                    alignSelf="center"
-                                    ms="2"
+                                {" "}
+                                <CloseButton
+                                    as="span"
+                                    role="button"
                                     size="2xs"
-                                    variant="ghost"
-                                    onClick={() => {
+                                    me="-2"
+                                    onClick={(e) => {
+                                        e.stopPropagation()
                                         removeTab(tab.id)
                                     }}
-                                >
-                                    <LuX/>
-                                </Button>
+                                />
                             </Tabs.Trigger>
                         })
                     }
@@ -78,92 +80,38 @@ const MatrixTabs = (props) => {
                 </Tabs.List>
                 {
                     tabs.map((tab, index) => {
-                        return <Tabs.Content value={tab.id}>
-                            Content TAB={tab.title}
+                        return <Tabs.Content
+                            key={"tabs_content_" + tab.id}
+                            value={tab.id}>
+                            <Grid
+                                key={"tabs_content_grid_"+tab.id}
+                                templateColumns="repeat(2, 1fr)"
+                                templateRows="repeat(2, 1fr)"
+                                gap={4}
+                                height="100%"
+                                minHeight="0" // Важно для вложенных Grid
+                            >
+                                {['Срочно и важно', 'Не срочно, но важно', 'Срочно, но не важно', 'Не срочно и не важно'].map((title, idx) => (
+                                    <GridItem
+                                        key={"tabs_content_grid_item_"+tab.id+"_"+idx}
+                                        bg={quadrantBg}
+                                        borderWidth="1px"
+                                        borderColor={quadrantBorder}
+                                        borderRadius="md"
+                                        p={4}
+                                        overflow="auto" // Добавляем скролл при необходимости
+                                    >
+                                        <Heading size="md" mb={4}>{title}</Heading>
+                                        {/* Место для будущих задач */}
+                                    </GridItem>
+                                ))}
+                            </Grid>
+
+
                         </Tabs.Content>
                     })
                 }
             </Tabs.Root>
-            {/* Панель вкладок */}
-            {/*<Tabs*/}
-            {/*    key={appData.tabs.length}*/}
-            {/*    index={activeTab}*/}
-            {/*    onChange={setActiveTab}*/}
-            {/*    variant="enclosed-colored"*/}
-            {/*    isLazy*/}
-            {/*    display="flex"*/}
-            {/*    flexDirection="column"*/}
-            {/*    flex="1"*/}
-            {/*    overflow="hidden"*/}
-            {/*>*/}
-            {/*<TabList overflowX="auto" overflowY="hidden">*/}
-            {/*    {appData.tabs.map((tab, index) => (*/}
-            {/*        <Tab*/}
-            {/*            key={tab.id}*/}
-            {/*            _selected={{color: 'white', bg: tab.color}}*/}
-            {/*            minWidth="150px"*/}
-            {/*            position="relative"*/}
-            {/*        >*/}
-            {/*            <Text isTruncated>{tab.title}</Text>*/}
-            {/*            <IconButton*/}
-            {/*                aria-label="Удалить вкладку"*/}
-            {/*                icon={<CloseIcon boxSize={2.5}/>}*/}
-            {/*                size="xs"*/}
-            {/*                ml={2}*/}
-            {/*                borderRadius="full"*/}
-            {/*                onClick={(e) => {*/}
-            {/*                    e.stopPropagation();*/}
-            {/*                    removeTab(index);*/}
-            {/*                }}*/}
-            {/*                _hover={{bg: 'rgba(0,0,0,0.1)'}}*/}
-            {/*            />*/}
-            {/*        </Tab>*/}
-            {/*    ))}*/}
-            {/*    <Tab onClick={addTab} _hover={{bg: 'gray.100'}}>*/}
-            {/*        <AddIcon boxSize={3}/>*/}
-            {/*    </Tab>*/}
-            {/*</TabList>*/}
-
-            {/*/!* Контент вкладок - теперь растягивается *!/*/}
-            {/*<TabPanels*/}
-            {/*    flex="1"*/}
-            {/*    minHeight="0" // Важно для корректного растягивания в Flex*/}
-            {/*    overflow="hidden"*/}
-            {/*>*/}
-            {/*    {appData.tabs.map((tab) => (*/}
-            {/*        <TabPanel*/}
-            {/*            key={tab.id}*/}
-            {/*            p={0}*/}
-            {/*            height="100%"*/}
-            {/*            overflow="hidden"*/}
-            {/*        >*/}
-            {/*            /!* Компонент матрицы - теперь занимает всю высоту *!/*/}
-            {/*            <Grid*/}
-            {/*                templateColumns="repeat(2, 1fr)"*/}
-            {/*                templateRows="repeat(2, 1fr)"*/}
-            {/*                gap={4}*/}
-            {/*                height="100%"*/}
-            {/*                minHeight="0" // Важно для вложенных Grid*/}
-            {/*            >*/}
-            {/*                {['Срочно и важно', 'Не срочно, но важно', 'Срочно, но не важно', 'Не срочно и не важно'].map((title, idx) => (*/}
-            {/*                    <GridItem*/}
-            {/*                        key={idx}*/}
-            {/*                        bg={quadrantBg}*/}
-            {/*                        borderWidth="1px"*/}
-            {/*                        borderColor={quadrantBorder}*/}
-            {/*                        borderRadius="md"*/}
-            {/*                        p={4}*/}
-            {/*                        overflow="auto" // Добавляем скролл при необходимости*/}
-            {/*                    >*/}
-            {/*                        <Heading size="md" mb={4}>{title}</Heading>*/}
-            {/*                        /!* Место для будущих задач *!/*/}
-            {/*                    </GridItem>*/}
-            {/*                ))}*/}
-            {/*            </Grid>*/}
-            {/*        </TabPanel>*/}
-            {/*    ))}*/}
-            {/*</TabPanels>*/}
-            {/*</Tabs>*/}
         </Flex>
     );
 };
